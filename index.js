77f81b7b5fa39da6cbd37d31289b3a456047b835
geoffroy moreaud <g.moreaud@gmail.com>
2023-09-26 07:19:55 +0200

    last version

--- index.js
+++ index.js
@@ -126,8 +126,121 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   // on récupère l'id cgv (a personaliser par ce que l'on veux). JS connait par défaut pour les checkboxs donc pas besoin de déclarer au préalable (pareille pour les bouttons)
   if (cgv.checked) {
-    alert("formulaire envoyé");
+    document.querySelector("form > div").innerHTML = `
+      <h3>Pseudo : ${pseudo}<h3/>
+      <h3>Langage préféré : ${language}</h3>
+    `;
   } else {
     alert("Veuillez accepter les CGV");
   }
 });
+
+// --------------------------------------------------------
+
+// Load event
+// Il se déclenche une fois que la page a été chargée
+
+window.addEventListener("load", () => {
+  console.log("Document Chargé !");
+});
+
+// --------------------------------------
+// slectionner plusieurs class en même temps
+
+const boxes = document.querySelectorAll(".box");
+console.log(boxes);
+
+// Foreach
+
+boxes.forEach((box) => {
+  box.addEventListener("click", (e) => {
+    e.target.style.transform = "scale(0.7)";
+  });
+});
+
+// -------------------------------------------------
+// Prioriser l'execution d'une fonction l'une avant l'autre
+// dans ce cas la 2ème fonction s'éxecutera en première
+
+// Bubbling
+document.body.addEventListener("click", () => {
+  console.log("click 1");
+});
+
+// Usecapture
+document.body.addEventListener(
+  "click",
+  () => {
+    console.log("click 2");
+  },
+  true
+);
+
+// ------------------------------------------------------------------
+// stop propagation
+
+questionContainer.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log("ciu");
+});
+
+// --------------------------------------
+// BOM
+// ouverture d'une page web au moment du chargement de la page
+// window.open("https://www.google.com/maps", "height=600, width=800")
+
+//évènements adossés à Window
+// alert("Hello");
+
+btn2.addEventListener("click", () => {
+  confirm("Are you sure ?");
+});
+
+// prompt
+let answer;
+btn1.addEventListener("click", () => {
+  answer = prompt("Entrez votre nom !");
+  // ici le += permet de ne pas écraser ce qu'il y avait déja dans ce container
+  questionContainer.innerHTML += "<h4>Bravo " + answer + "</h4>";
+});
+
+// sert à déclencher une action à rebours
+setTimeout(() => {
+  // logique à exécuter
+  questionContainer.style.borderRadius = "300px";
+}, 2000);
+
+// injecte toute les 1 seconde une nouvelle boite au body
+
+// let interval = setInterval(() => {
+//   document.body.innerHTML += `<div class='box'><h2>Nouvelle boite !</h2></div>`;
+// }, 1000);
+
+// permet d'arreter l'interval au click sur le body
+
+// document.body.addEventListener("click", () => {
+//   clearInterval(interval);
+// });
+
+// location
+// savoir sur quel lien se trouve l'utilisateur
+// console.log(location.href);
+// console.log(location.host);
+// console.log(location.pathname);
+// // permet de savoir ce qu'a cherché l'utilisateur
+// console.log(location.search);
+
+// history
+// permet de consulter l'historique de navigation
+console.log(history);
+
+// permet de faire revenir l'utilisateur a la page précédente par exemple après la complétion d'un formulaire
+// window.history.back();
+
+// SetProperty
+
+// injecter du style dans des variables au préalablement déclarées en css
+// window.addEventListener("mousemove", (e) => {
+//   nav.style.setProperty("--x", e.layerX + "px");
+//   nav.style.setProperty("--y", e.layerY + "px");
+// });
